00b9f48c2c9d78968d7bcc40a1fae1a2396a39dc
admin <admin@email.com>
2025-10-16 17:49:14 +0900

    crypto

---
+++ crypto.js
@@ -0,0 +1,39 @@
+// crypto.js
+const crypto = require("crypto");
+
+let cryptPasswd = crypto
+  .createHash("sha256")
+  .update("sample123")
+  .digest("base64");
+
+console.log(cryptPasswd);
+
+// 1. DB의 값을 암호화값 vs. 사용자 입력한 값 암호화값 => 비교후 판별.
+let fixedSalt =
+  "qdLbMVVOCmPYBQcnOkUSMyREwSkzvU6ekxszXV3mWCZS2XSXxO1LV0YyfUB2lOHjU5KNdWuSf3T1Qkw0X5Q8rg==";
+
+async function getCryptoPassword(password) {
+  // 1. salting 임의의 구문. => 동일한 평문(비밀번호) -> 다른 암호값.
+  // let salt = crypto.randomBytes(64).toString("base64");
+  let dbPass =
+    "ylhQLM9dRTBA3RVJjnZnKgaKt0DmjORdR2bDnb43aiLHjSUd9ijndtl0N37+ReAtK+efewkcikq8iiWVfJpJiw==";
+
+  return new Promise((resolve, reject) => {
+    crypto.pbkdf2(password, fixedSalt, 100000, 64, "sha512", (err, key) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // console.log(dbPass == key.toString("base64") ? "same" : "different");
+      resolve(dbPass == key.toString("base64") ? "same" : "different");
+    });
+  });
+}
+
+getCryptoPassword("sample123")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
