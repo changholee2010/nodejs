3b81f785c7c07ea37470da6ef63457759e1bfd9d
admin <admin@email.com>
2025-10-16 12:48:54 +0900

    gitignore

---
+++ promise.js
@@ -0,0 +1,29 @@
+// promise.js
+// pending/ fulfilled/ rejected , then()/catch()
+const promise = new Promise(function (resolve, reject) {
+  // 정상완료=> 첫번째 매개값으로 받은 함수호출.
+  // 거부=> 두번째 매개값으로  받은 함수호출.
+  try {
+    console.log("hhhh");
+    setTimeout(function () {
+      resolve({ retCode: "Success", retVal: ["hong", "kim", "park"] });
+    }, 1000);
+  } catch (err) {
+    reject(new Error("Error !!!"));
+  }
+});
+
+promise
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+
+fetch("")
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
